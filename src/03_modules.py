44c153a0836fd5f69564c72dc2576f6a54094328
jjgorton <jjgorton@gmail.com>
2019-12-02 18:44:57 -0500

    completed 03

--- src/03_modules.py
+++ src/03_modules.py
@@ -5,27 +5,44 @@ methods, and the os module, which gives you access to lower-
 level operating system functionality.
 """
 
+import getpass
+import os
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+for arg in sys.argv:
+    print(arg)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
 
+platform = sys.platform
+print('OS name: ' + platform)
+
 # Print out the version of Python you're using:
 # YOUR CODE HERE
 
+print('Python version: ' + sys.version)
 
-import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
 
+print('Current Process ID: ' + str(os.getpid()))
+
 # Print the current working directory (cwd):
 # YOUR CODE HERE
 
+print('CWD: ' + os.getcwd())
+
 # Print out your machine's login name
 # YOUR CODE HERE
+
+print('Login Name: ' + os.getlogin())
+
+# Preferred method:
+
+print('Using preferred method to get username: ' + getpass.getuser())
