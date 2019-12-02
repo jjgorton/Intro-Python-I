c6d32b586a329348a3867e35f22d45af8a38f14c
jjgorton <jjgorton@gmail.com>
2019-12-02 16:33:36 -0500

    completed 02

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -15,7 +15,15 @@ y = "7"
 
 # YOUR CODE HERE
 
+print(x + int(y))
+
+# # or this
+z = x + int(y)
+print(z)
+
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+
+print(str(x) + y)
